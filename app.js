3c8028229b166b7b14a3b1b233bc1ebeae15e3b0
TorresLeandroMartin <leandromartintorres17797@gmail.com>
2022-12-14 21:31:56 -0300

    primer commit 1

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// const publicPath= path.resolve(__dirname, './public')
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'views/homeV1.2.html'));
+
+})
+
+app.get('/registro', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'views/registerV0.html'));
+
+})
+
+app.get('/login', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'views/loginV0.html'));
+
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Corriendo en puerto ' + port));
+
+// funciona?
